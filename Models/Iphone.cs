ade250d759385ccd3580b621cb9349d4f95a0fe2
Vitória <101277316+vivikamizono@users.noreply.github.com>
2023-12-29 17:07:15 -0300

    Update Iphone.cs

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){
+            
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+             Console.WriteLine($"Instalando... {nomeApp}");
+            Console.WriteLine($"{nomeApp } instalado com sucesso!");
+        }
+        // TODO: Sobrescrever o método "InstalarAplicativo"1
     }
 }

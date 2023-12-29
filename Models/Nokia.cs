712e42494661a0db324a12145a7483edbe06cb31
Vitória <101277316+vivikamizono@users.noreply.github.com>
2023-12-29 17:07:31 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando... {nomeApp}");
+            Console.WriteLine($" {nomeApp} instalado com sucesso!");
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }

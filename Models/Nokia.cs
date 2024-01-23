42ec53eb3e73d59a76fd29a574726dd1dff2309e
Vitória <101277316+vivikamizono@users.noreply.github.com>
2024-01-23 17:34:28 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,41 +1,18 @@
 namespace DesafioPOO.Models
 {
-    public abstract class Smartphone
+    // TODO: Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        public string Numero {get; set; }
-        private string Modelo {get; set; }
-        private string Imei {get; set; }
-        private int Memoria {get; set; }
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){
 
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero, string modelo, string imei, int memoria)
-        {
-            Numero = numero;
-            Modelo = modelo;
-            Imei = imei;
-
-
-            
-            Memoria = memoria;
-
-            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
-        public void Ligar()
-        {
-            Console.WriteLine("Digite o numero: ");
-            Numero = Console.ReadLine();
-
-            Console.WriteLine("Ligando...");
-            Console.ReadKey();
+        public override void InstalarAplicativo(string nomeApp)
+        { 
+        // TODO: Sobrescrever o método "InstalarAplicativo"
+            Console.WriteLine($"Instalando... {nomeApp}");
+            Console.WriteLine($" {nomeApp} instalado com sucesso!");
         }
-
-        public void ReceberLigacao()
-        {
-            Console.WriteLine("Recebendo ligação...");
-        }
-
-        public abstract void InstalarAplicativo(string nomeApp);
+       
     }
 }

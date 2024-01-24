84a28e265b35245c610d4a43c515726cb394f580
Vitória <101277316+vivikamizono@users.noreply.github.com>
2024-01-24 14:07:22 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,115 @@
 using DesafioPOO.Models;
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo A", imei: "10101", memoria: 120 );
+        Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo B", imei: "01010", memoria: 210 );
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+        Console.WriteLine("Smartphone Nokia: ");
+        Console.WriteLine("Escolha sua opção: \n 1- Realizar Ligação. \n 2- Enviar Mensagem \n 3- Instalar Aplicativo \n 4- Desinstalar aplicativo.");
 
-Console.WriteLine("Smartphone Nokia: ");
-Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo A", imei: "10101", memoria: 120 );
-nokia.Ligar();
-nokia.InstalarAplicativo("Instagram");
+        string escolha = Console.ReadLine();
 
-Console.WriteLine("\n");
+        if (escolha == "1")
+        {
+            RealizarLigacao();
+            Console.WriteLine("Smartphone Iphone: ");
+            iphone.ReceberLigacao();
+        }
 
-Console.WriteLine("Smartphone Iphone: ");
-Smartphone iphone = new Iphone(numero: "123456", modelo: "Modelo B", imei: "01010", memoria: 210 );
-iphone.ReceberLigacao();
-iphone.InstalarAplicativo("Pinterest");
+        else if (escolha == "2")
+        {
+            EnviarMensagem();
+            Console.WriteLine("Smartphone Iphone: ");
+            iphone.ReceberMenssagem();
+        }
+
+        else if (escolha == "3")
+        {   
+        Console.WriteLine("Escolha seu smartphone: \n 1- Nokia \n 2- Iphone");
+        string escolhaSmartphone = Console.ReadLine();
+
+        Smartphone smartphone;
+
+        if (escolhaSmartphone == "1")
+        {
+            smartphone = new Nokia(numero: "123456", modelo: "Modelo A", imei: "01011", memoria: 256);
+             InstalarAplicativo();
+            nokia.InstalarAplicativo("Instagram no modelo Nokia");
+        }
+
+
+        else if (escolhaSmartphone == "2")
+        {
+            smartphone = new Iphone(numero: "654321", modelo: "Modelo B", imei: "01010", memoria: 210);
+             InstalarAplicativo();
+            iphone.InstalarAplicativo("WhatsApp no modelo Iphone");
+        }
+
+
+        else
+        {
+            Console.WriteLine("Opção inválida. Encerrando programa.");
+            return;
+        }
+        
+        }
+        else if (escolha == "4")
+        {
+            Console.WriteLine("Escolha seu smartphone: \n 1- Nokia \n 2- Iphone");
+            string escolhaSmartphone = Console.ReadLine();
+
+        Smartphone smartphone;
+
+        if (escolhaSmartphone == "1")
+        {
+            smartphone = new Nokia(numero: "123456", modelo: "Modelo A", imei: "01011", memoria: 256);
+            DesinstalarAplicativo();
+            nokia.DesinstalarAplicativo("Instagram no modelo Nokia");
+        }
+
+
+        else if (escolhaSmartphone == "2")
+        {
+            smartphone = new Iphone(numero: "654321", modelo: "Modelo B", imei: "01010", memoria: 210);
+            DesinstalarAplicativo();
+            iphone.DesinstalarAplicativo("WhatsApp no modelo Iphone");
+        }
+
+
+        else
+        {
+            Console.WriteLine("Opção inválida. Encerrando programa.");
+            return;
+        }
+        
+        }
+
+
+        else
+        {
+            Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+        }
+    }
+
+    static void RealizarLigacao()
+    {
+        Console.WriteLine("Modelo Nokia realizando ligação...");
+    }
+
+    static void EnviarMensagem()
+    {
+        Console.WriteLine("Enviando mensagem...");
+    }
+
+    static void InstalarAplicativo()
+    {
+        Console.WriteLine("Instalando aplicativo...");
+    }
+
+    static void DesinstalarAplicativo()
+    {
+        Console.WriteLine("Desinstalando aplicativo...");
+    }
+}

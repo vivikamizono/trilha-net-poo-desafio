dc90115f4a3e161ccf9603f526411fb87363e8aa
Vitória <101277316+vivikamizono@users.noreply.github.com>
2024-01-24 14:06:59 -0300

    Update Smartphone.cs

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,21 +1,22 @@
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
-    { 
-   
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+    {
         public string Numero {get; set; }
         private string Modelo {get; set; }
         private string Imei {get; set; }
         private int Memoria {get; set; }
+
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-       
-            // TODO: Passar os parâmetros do construtor para as propriedades
             Numero = numero;
             Modelo = modelo;
             Imei = imei;
             Memoria = memoria;
+
+            // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
@@ -26,11 +27,31 @@ namespace DesafioPOO.Models
             Console.WriteLine("Ligando...");
         }
 
+        
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
 
+        public void Menssagem()
+        {
+            Console.WriteLine("Digite o numero: ");
+            Numero = Console.ReadLine();
+
+            Console.WriteLine("Enviando menssagem de texto...");
+        }
+
+         public void ReceberMenssagem()
+        {
+            Console.WriteLine("Recebendo Menssagem de texto!! ");
+            Numero = Console.ReadLine();
+
+            Console.WriteLine("Enviando menssagem de texto...");
+        }
+
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public abstract void DesinstalarAplicativo(string nomeApp);
+
     }
 }
